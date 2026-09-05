0f3aa5574894146dc45a7394e506621349f4eee1
Guilherme Souza <dev.guih@gmail.com>
2026-09-05 13:11:37 -0300

    feat: novos audios ao usar o teclado e confirmar voto

--- urna/src/componentes/votacao/TecladoVotacao.vue
+++ urna/src/componentes/votacao/TecladoVotacao.vue
@@ -1,16 +1,37 @@
 <script setup lang="ts">
+import audioBotaoUrna from "../../assets/audios/botao.mp3";
+
 const numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+const audioBotao = new Audio(audioBotaoUrna);
 
 defineProps<{
   podeConfirmar: boolean
 }>()
 
-defineEmits<{
+const emitir = defineEmits<{
   digito: [valor: string]
   branco: []
   corrigir: []
   confirmar: []
 }>()
+
+function tocarAudioBotao() {
+  audioBotao.currentTime = 0;
+  void audioBotao.play().catch(() => undefined);
+}
+
+function acionarBotao(evento: "branco" | "corrigir" | "confirmar") {
+  tocarAudioBotao();
+
+  if (evento === "branco") emitir("branco");
+  else if (evento === "corrigir") emitir("corrigir");
+  else emitir("confirmar");
+}
+
+function inserirDigito(numero: string) {
+  tocarAudioBotao();
+  emitir("digito", numero);
+}
 </script>
 
 <template>
@@ -27,24 +48,24 @@ defineEmits<{
         type="button"
         :class="{ 'tecla-zero': numero === '0' }"
         :aria-label="`Digitar ${numero}`"
-        @click="$emit('digito', numero)"
+        @click="inserirDigito(numero)"
       >
         {{ numero }}
       </button>
     </div>
 
     <div class="teclas-acao">
-      <button class="tecla-acao tecla-branco" type="button" @click="$emit('branco')">
+      <button class="tecla-acao tecla-branco" type="button" @click="acionarBotao('branco')">
         <span>Branco</span>
       </button>
-      <button class="tecla-acao tecla-corrige" type="button" @click="$emit('corrigir')">
+      <button class="tecla-acao tecla-corrige" type="button" @click="acionarBotao('corrigir')">
         <span>Corrige</span>
       </button>
       <button
         class="tecla-acao tecla-confirma"
         type="button"
         :disabled="!podeConfirmar"
-        @click="$emit('confirmar')"
+        @click="acionarBotao('confirmar')"
       >
         <span>Confirma</span>
       </button>
