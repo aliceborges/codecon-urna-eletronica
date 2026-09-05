e306f71ab5ef2fe43858589ab072c206c92f9c02
Guilherme Souza <dev.guih@gmail.com>
2026-09-05 13:00:02 -0300

    feat: bloquear double touch

--- urna/src/paginas/PaginaSucesso.vue
+++ urna/src/paginas/PaginaSucesso.vue
@@ -1,33 +1,28 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-import CabecalhoUrna from '../componentes/estrutura/CabecalhoUrna.vue'
-import AvisoPrivacidade from '../componentes/estrutura/AvisoPrivacidade.vue'
-import ConfirmacaoVoto from '../componentes/sucesso/ConfirmacaoVoto.vue'
-import { reiniciarSessaoVotacao, ultimoTipoVoto } from '../estado/sessaoVotacao'
+import { useRouter } from "vue-router";
+import CabecalhoUrna from "../componentes/estrutura/CabecalhoUrna.vue";
+import AvisoPrivacidade from "../componentes/estrutura/AvisoPrivacidade.vue";
+import ConfirmacaoVoto from "../componentes/sucesso/ConfirmacaoVoto.vue";
+import {
+  reiniciarSessaoVotacao,
+  ultimoTipoVoto,
+} from "../estado/sessaoVotacao";
 
-const roteador = useRouter()
+const roteador = useRouter();
 
 function prepararProximoEleitor() {
-  reiniciarSessaoVotacao()
-  void roteador.replace({ name: 'votacao' })
+  reiniciarSessaoVotacao();
+  void roteador.replace({ name: "votacao" });
 }
 </script>
 
 <template>
   <main class="estrutura-pagina pagina-sucesso">
-    <section class="urna-eletronica urna-sucesso" aria-labelledby="titulo-sucesso">
-      <CabecalhoUrna titulo="Urna eletrônica">
-        <template #acao>
-          <button
-            class="botao-exportar-cabecalho"
-            type="button"
-            @click="roteador.push({ name: 'exportacao' })"
-          >
-            <span class="indicador-status" aria-hidden="true"></span>
-            Exportar boletim
-          </button>
-        </template>
-      </CabecalhoUrna>
+    <section
+      class="urna-eletronica urna-sucesso"
+      aria-labelledby="titulo-sucesso"
+    >
+      <CabecalhoUrna titulo="Urna eletrônica" />
       <ConfirmacaoVoto
         :tipo-voto="ultimoTipoVoto"
         @proximo-eleitor="prepararProximoEleitor"
